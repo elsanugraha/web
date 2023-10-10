74b7451c6cbcd014d715eb6aa22006edb8468978
elsanugraha <141709007+elsanugraha@users.noreply.github.com>
2023-10-10 17:31:11 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,8 @@
+let nama = "Elsa Nugraha";
+document.getElementById("nama").innerHTML = nama
+let alamat = "pelang";
+document.getElementById("alamat").innerHTML = alamat
+let ttg = "tasikmalaya";
+document.getElementById("ttg").innerHTML = ttg
+let hoby = "turu";
+document.getElementById("hoby").innerHTML = hoby
